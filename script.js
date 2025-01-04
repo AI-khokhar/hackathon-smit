5ec3e0c8c812b93bf0245384f2f7fcb52790fef5
Asiya Ikhlaq <ikhlaqasiya7@gmail.com>
2025-01-04 00:07:13 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+
+        
+        
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+
+
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+      apiKey: "AIzaSyBk16IjGxe1MCLFoo8logapXb3MA-VUVHo",
+      authDomain: "hackathon-1822a.firebaseapp.com",
+      projectId: "hackathon-1822a",
+      storageBucket: "hackathon-1822a.firebasestorage.app",
+      messagingSenderId: "22724491091",
+      appId: "1:22724491091:web:010efdff22988008a853a6"
+    };
+  
+     // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  constfirebaseConfig = {
+    apiKey: "AIzaSyBHt7bQw2ACm5Vii5fDc-Je_CGCVRLSaGQ",
+    authDomain: "contact-155aa.firebaseapp.com",
+    projectId: "contact-155aa",
+    storageBucket: "contact-155aa.firebasestorage.app",
+    messagingSenderId: "649511281687",
+    appId: "1:649511281687:web:2066700be279cfa474a852",
+    measurementId: "G-ZVXCS95B1F"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+
+
+  const firebaseConfigration = {
+    apiKey: "AIzaSyBwLrhLXIUWZHHpIhN6QtuccuEocKbWdFo",
+    authDomain: "hackathon-1-4f5c9.firebaseapp.com",
+    projectId: "hackathon-1-4f5c9",
+    storageBucket: "hackathon-1-4f5c9.firebasestorage.app",
+    messagingSenderId: "268061829245",
+    appId: "1:268061829245:web:6abbc93a5c0b848e6f6914",
+    measurementId: "G-NSHD931RJ0"
+  };
+  
+
+  
